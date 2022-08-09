da5fb1f7889cd9a4b23b4dc3a2a01c6f25f4b551
Hyekyung <mwjer333@gmail.com>
2022-08-09 17:54:17 +0900

    API 서버에 POST로 저장하는 기능 구현

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import pymysql
+
+app = Flask(__name__)
+db = pymysql.connect(host='localhost', port=3306, user='root', passwd='1234', db='voice_ai', charset="utf8")
+cursor = db.cursor()
+
+@app.route("/api", methods=["GET"])
+def get_text():
+	sql = "select * from stt_text"
+	cursor.execute(sql)
+	results = cursor.fetchall()
+	db.commit()
+	# print(results)
+	# results = ((1, '안녕하세요'), (2, 'Hello'))
+	stts = []
+	for result in results:
+		stts.append({
+			'id': result[0],
+			'stt_text': result[1]
+		})
+	return jsonify(stts)
+
+@app.route("/api", methods=["POST"])
+def save_text():
+	server_text = request.form["stttext"]
+	sql = "insert into stt_text (stttext) values ('%s')" %(server_text)
+	cursor.execute(sql)
+	db.commit()
+	return "save OK"
+
+if __name__ == "__main__":
+    app.run(debug=True)
