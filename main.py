1790dd2e09b44ac1a4c23b74e1753f5f288b2587
Hyekyung <mwjer333@gmail.com>
2022-08-11 19:56:43 +0900

    revised method of storing in (/api/stt_voice) method=[POST], 'file_name'=request.form[] -> uuid.uuid4() ./voice_files에 음악파일 1개 들어감(추후 삭제예정)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 import pymysql
 from settings import MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB
+import uuid
 
 
 db = pymysql.connect(host='localhost', port=3306, user=MYSQL_USER, passwd=MYSQL_PASSWORD, db=MYSQL_DB, charset="utf8")
@@ -27,7 +28,7 @@ def upload_file():
 	if 'file' not in request.files:
 		return "No file found"
 	file = request.files['file']
-	file_name = request.form['file_name']
+	file_name = uuid.uuid4()
 	file.save("""voice_files/%s.wav""" % file_name)
 	return "file successfully saved"
 
