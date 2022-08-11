8691895287847dbb63134e71663455196c73bac9
billiamchoi <billiamchoi4u@gmail.com>
2022-08-11 11:50:26 +0900

    stt_voice api 및 .env 추가

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask import request
 import pymysql
+from settings import MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB
+
+
+db = pymysql.connect(host='localhost', port=3306, user=MYSQL_USER, passwd=MYSQL_PASSWORD, db=MYSQL_DB, charset="utf8")
+cursor = db.cursor()
+UPLOAD_DIRECTORY = "./voice_files"
 
 app = Flask(__name__)
-db = pymysql.connect(host='localhost', port=3306, user='root', passwd='1234', db='voice_ai', charset="utf8")
-cursor = db.cursor()
 
-@app.route("/api", methods=["GET"])
-def get_text():
-	sql = "select * from stt_text"
-	cursor.execute(sql)
-	results = cursor.fetchall()
-	db.commit()
-	# print(results)
-	# results = ((1, '안녕하세요'), (2, 'Hello'))
-	stts = []
-	for result in results:
-		stts.append({
-			'id': result[0],
-			'stt_text': result[1]
-		})
-	return jsonify(stts)
+@app.route("/", methods = ["GET"])
+def main():
+	return "ok"
 
-@app.route("/api", methods=["POST"])
-def save_text():
-	server_text = request.json["stttext"]
-	sql = "insert into stt_text (stttext) values ('%s')" %(server_text)
+@app.route("/api/stt_text", methods=["POST"])
+def create_stt_text():
+	text = request.json["text"]
+	sql = """ insert into stt_text (text) values ('%s') """ % text
 	cursor.execute(sql)
 	db.commit()
-	return "save OK"
+	return "OK"
+
+@app.route("/api/stt_voice", methods=["POST"])
+def upload_file():
+	if 'file' not in request.files:
+		return "No file found"
+	file = request.files['file']
+	file_name = request.form['file_name']
+	file.save("""voice_files/%s.wav""" % file_name)
+	return "file successfully saved"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
+
