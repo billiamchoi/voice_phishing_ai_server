9dca155fb36d2f69c1b4e3936eb5a6e962a450df
billiamchoi <billiamchoi4u@gmail.com>
2022-08-24 16:40:06 +0900

    stt_voice_seg api 구현:x초마다 client에서 음성wav파일 서버로 저장하는 기능

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from flask import Flask
 from flask import request
 import pymysql
 from settings import MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB
-import uuid
+import os
 import scripts.text_lstm_model as text_model
 
 db = pymysql.connect(host='localhost', port=3306, user=MYSQL_USER, passwd=MYSQL_PASSWORD, db=MYSQL_DB, charset="utf8")
 cursor = db.cursor()
+db.autocommit(True)
 UPLOAD_DIRECTORY = "./voice_files"
-
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET"])
@@ -37,15 +37,31 @@ def create_stt_text_seg():
 	score = str(text_model.predict_text(text)[0][0])
 	return score
 
-
 @app.route("/api/stt_voice", methods=["POST"])
-def upload_file():
+def create_stt_voice():
 	if 'file' not in request.files:
 		return "No file found"
 	file = request.files['file']
-	file_name = uuid.uuid4()
-	file.save("""voice_files/%s.wav""" % file_name)
+	file_name = request.form['file_name']
+	file.save("""voice_files/whole/%s.wav""" % file_name)
 	return "file successfully saved"
+
+@app.route("/api/stt_voice_seg", methods=["POST"])
+def create_stt_voice_seg():
+	if 'file' not in request.files:
+		return "No file found"
+	file = request.files['file']
+	file_name = request.form['file_name']
+	directory_name = request.form['directory_name']
+	file.save("""voice_files/segment/%s/%s.wav""" % (directory_name, file_name))
+	return "file successfully saved"
+
+@app.route("/api/start_record", methods=["POST"])
+def start_record():
+	file_name = request.json['file_name']
+	os.mkdir("""voice_files/segment/%s""" % file_name)
+	return "ok"
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
