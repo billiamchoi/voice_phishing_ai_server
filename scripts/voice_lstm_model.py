c4e66fa676edb01e7dcd5d21eef99bd0125117fb
billiamchoi <billiamchoi4u@gmail.com>
2022-09-13 21:19:20 +0900

    보이스 모델 서버에 적용

--- scripts/voice_lstm_model.py
+++ scripts/voice_lstm_model.py
@@ -3,7 +3,7 @@ from keras.models import load_model
 import librosa
 import numpy as np
 
-model = load_model(filepath='models/model3.h5')
+model = load_model(filepath='models/model23.h5')
 
 def padding(mel_spec, sr, second):
     #501 이해하고 dynamic 하게 짜야됨
@@ -13,7 +13,7 @@ def padding(mel_spec, sr, second):
     else:
         # 여기 501도 mfcc 조합에 따라 다 바뀌어야 함
         result = mel_spec[:,0:501]
-    return result.reshape(-1,20040)
+    return result
 
 def predict_voice_mfcc(wav_file, sr, n_mfcc, second):
     sr = int(sr)
@@ -32,13 +32,12 @@ def predict_voice_mel(wav_file, sr, n_mels, second):
     result_mel = padding(mel, sr, second)
     result_mel = np.expand_dims(result_mel, axis=0)
     pre_result_mel = model.predict(result_mel)
-    pre_result_mel = pre_result_mel.reshape(-1,20040)
-    result_mel = result_mel.reshape(-1,20040)
+    pre_result_mel = pre_result_mel.reshape(-1, 20040)
+    result_mel = result_mel.reshape(-1, 20040)
     mse = np.mean(np.power(result_mel - pre_result_mel, 2))
-    error_df = pd.DataFrame({'Reconstruction_error': mse})
-    vo_threshold_fixed = 267
-    ab_pred = [1 if e > vo_threshold_fixed else 0 for e in error_df['Reconstruction_error'].values]
-    if ab_pred == [1]:
+    vo_threshold_fixed = 2000
+    if(mse > vo_threshold_fixed):
         return 1
     else:
         return 0
+
